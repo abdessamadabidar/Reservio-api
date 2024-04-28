43a68032f095d8268c44eeff5e5fc88cb651abfe
Abdessamad Abidar <abdessamad.abidar@hotmail.com>
2024-04-28 23:51:35 +0100

    refactore database relations & add new migration

---
+++ Migrations/20240428224730_RefactorDatabaseRelations.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservio.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorDatabaseRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoleUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Roles",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Roles_Users_UserId",
+                table: "Roles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Roles_Users_UserId",
+                table: "Roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Roles");
+
+            migrationBuilder.CreateTable(
+                name: "RoleUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleUsers", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_RoleUsers_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleUsers_RoleId",
+                table: "RoleUsers",
+                column: "RoleId");
+        }
+    }
+}
